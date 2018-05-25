bdbed93be19be4855573ed35750df020bf788e98
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-05-25 13:59:09 +0800

    Splash work

---
+++ app/src/main/java/com/cogitator/firebasemlkit/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.cogitator.firbasemlkit
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Window
+import android.view.WindowManager
+
+/**
+ * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 25/05/2018 (MM/DD/YYYY)
+ */
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_spalsh)
+    }
+}
---
+++ app/src/main/java/com/cogitator/firebasemlkit/text_recognizer/TextRecognizerActivity.kt
@@ -0,0 +1,9 @@
+package com.cogitator.firbasemlkit.text_recognizer
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 25/05/2018 (MM/DD/YYYY)
+ */
+class TextRecognizerActivity : AppCompatActivity() {
+}
