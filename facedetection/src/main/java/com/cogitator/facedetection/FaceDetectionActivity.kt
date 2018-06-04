403e8b40ca86dbee7a5e89051bb97f3cbf2a26de
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2018-06-04 15:49:12 +0800

    working on face detection

--- facedetection/src/main/java/com/cogitator/facedetection/FaceDetectionActivity.kt
+++ facedetection/src/main/java/com/cogitator/facedetection/FaceDetectionActivity.kt
@@ -1,7 +1,9 @@
 package com.cogitator.facedetection
 
+import android.support.v7.app.AppCompatActivity
+
 /**
  * @author Ankit Kumar (ankitdroiddeveloper@gmail.com) on 04/06/2018 (MM/DD/YYYY)
  */
-class FaceDetectionActivity {
+class FaceDetectionActivity : AppCompatActivity() {
 }
